96d4768def607a9538d3eaf8ddadca3b3a08a22a
VadymKhludieiev <156354412+VadymKhludieiev@users.noreply.github.com>
2024-03-30 21:34:48 +0100

    next

--- js/main.js
+++ js/main.js
@@ -10,3 +10,14 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
   });
+
+
+  const swiperAbout = new Swiper(".about-slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  });
